9e84f98c429100f6253af04c3a694264c8490a8c
Gideon <kipkerring.gedion@gmail.com>
2023-12-19 09:11:11 +0300

    handling the ServicePointController Logic

--- Queue Management System/Queue Management System/Controllers/ServicePointController.cs
+++ Queue Management System/Queue Management System/Controllers/ServicePointController.cs
@@ -1,19 +1,65 @@
-﻿namespace Queue_Management_System.Controllers
+﻿using Queue_Management_System.Models;
+using System;
+namespace Queue_Management_System.Controllers
 {
     public class ServicePointController
     {
-        private Services services;
+        private ServicesModel servicesModel;
 
-        public ServicePointController(Services services)
+        public ServicePointController(ServicesModel servicesModel)
         {
-            this.services = services;
+            this.servicesModel = servicesModel;
         }
 
-        public Tickets ProcessNextTicket()
+        public TicketsModel ProcessNextTicket()
         {
-            var nextTicket = services.Dequeue();
-            //logic to handle the next ticket
+            var nextTicket = servicesModel.Dequeue();
+
+            // Logic to handle the next ticket
+            if (nextTicket != null)
+            {
+                Console.WriteLine($"Processing ticket {nextTicket.Number} for service {nextTicket.Service}");
+                // Additional processing logic can go here
+            }
+            else
+            {
+                Console.WriteLine("No tickets available for processing");
+            }
+
             return nextTicket;
+        }
+
+        public void MarkAsNoShow(TicketsModel ticket)
+        {
+            // Logic to mark a ticket as a no-show
+            Console.WriteLine($"Marking ticket {ticket.Number} as a no-show");
+            // To include more Logic
+        }
+
+        public void MarkAsFinished(TicketsModel ticket)
+        {
+            // Logic to mark a ticket as finished
+            Console.WriteLine($"Marking ticket {ticket.Number} as finished");
+            // To include more Logic
+        }
+
+        public void TransferTicket(TicketsModel ticket, ServicePointController targetServicePoint)
+        {
+            // Logic to transfer a ticket to another service point
+            Console.WriteLine($"Transferring ticket {ticket.Number} to {targetServicePoint}");
+            targetServicePoint.ProcessNextTicket();
+            // To include more Logic
+        }
+
+        public void ViewQueue()
+        {
+            // Logic to view the current queue
+            var queue = servicesModel.EnQueue();
+            Console.WriteLine("Current Queue:");
+            foreach (var queuedTicket in queue)
+            {
+                Console.WriteLine(value: $"Ticket {queuedTicket.Number} - Service {queuedTicket.Service}");
+            }
         }
     }
 }
---
+++ Queue Management System/Queue Management System/Controllers/ServicePointControllerBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Queue_Management_System.Controllers
+{
+    public class ServicePointControllerBase
+    {
+
+        public TicketsModel ProcessNextTicket()
+        {
+            var nextTicket = servicesModel.Dequeue();
+            //logic to handle the next ticket
+            return nextTicket;
+        }
+    }
+}
